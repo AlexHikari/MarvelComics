a0788ebdc2b114cef8709c8ac2f88aa493f98bfc
Alejandro Rivas Moreira <alejandro.rivasmoreira@worldline.com>
2018-10-29 13:02:18 +0100

    - Finished getCharacter usecase

--- app/src/main/java/com/example/marvelcomics/usecases/GetCharacter.kt
+++ app/src/main/java/com/example/marvelcomics/usecases/GetCharacter.kt
@@ -3,25 +3,23 @@ package com.example.marvelcomics.usecases
 import com.example.marvelcomics.data.ComicRepository
 import com.example.marvelcomics.domain.Character
 import io.reactivex.Single
-
 import io.reactivex.disposables.Disposable
 import io.reactivex.observers.DisposableSingleObserver
 
 class GetCharacter(private val comicRepository: ComicRepository) {
 
-    operator fun invoke(name: String): Character {
+    operator fun invoke(name: String, onSuccess: (Character) -> Unit, onError: (Throwable) -> Unit) {
+        val characterSingle: Single<Character> = comicRepository.getCharacterByName(name)
+        val disposable: Disposable = characterSingle.subscribeWith(object : DisposableSingleObserver<Character>() {
+            override fun onSuccess(character: Character) {
+                onSuccess(character)
+            }
 
-        val characterSingle: Single<Character> =  comicRepository.getCharacterByName(name)
-         val disposable: Disposable = characterSingle.subscribeWith(object: DisposableSingleObserver<Character>(){
-             override fun onSuccess(t: Character) {
-                 TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-             }
+            override fun onError(e: Throwable) {
+                onError(e)
+            }
 
-             override fun onError(e: Throwable) {
-                 TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-             }
-
-         })
+        })
 
     }
 }
